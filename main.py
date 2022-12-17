9543a8905801e5c32e9988c9a26dc4e83b5e1261
Abdel-Waheb <103281009+Lakkas216@users.noreply.github.com>
2022-12-17 23:49:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ https://github.com/Lakkas216
 
 import ranking
 import odds
+import matches
 
 if __name__ == "__main__":
     ranking.get_ranking_all()
     odds.get_odds_all()
+    matches.matches_by_day()
+
